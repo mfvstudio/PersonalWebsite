6761b3c0ffb68a7d390f8aff70396f80aa2bd328
Mario Flores Vences <mfvdev@icloud.com>
2025-06-12 09:57:52 -0700

    Setting up eslint and git hooks.

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,14 @@
 </script>
 
 <template>
-    <h1>Hello World</h1>
-    <div class="xl:mt-96">
-        <div class="left-title">mfv</div>
-        <div class="right-title flex justify-end mt-100">dev</div>
-    </div>
-    <svg width="100000" height="100000" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="2" height="60" x="0" y="0" fill="yellow" class="transition-title"/>
-    </svg>  
+    <div>
+        <h1>Hello World</h1>
+        <div class="xl:mt-96">
+            <div class="left-title">mfv</div>
+            <div class="right-title flex justify-end mt-100">dev</div>
+        </div>
+        <svg width="100000" height="100000" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="2" height="60" x="0" y="0" fill="yellow" class="transition-title"/>
+        </svg>
+    </div> 
 </template>
