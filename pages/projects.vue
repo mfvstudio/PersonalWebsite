ea616abe3357d960c2ef662fcce16b78575967f8
Mario Flores Vences <mfvdev@icloud.com>
2025-06-17 09:38:50 -0700

    Small change to projects page. Added coming soon text.

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis id maxime ut omnis non nostrum dolorum ab tenetur placeat voluptatum sunt, et perspiciatis tempora iste quia accusantium ipsam corporis hic.
+  <div class="text-center mt-80 text-5xl">
+    Coming Soon
   </div>
 </template>
 
